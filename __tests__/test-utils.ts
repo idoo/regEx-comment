de06015a637955fd2f3d28dd44e359e8bb773a51
Ivan Verevkin <ivan.p.verevkin@gmail.com>
2019-08-27 16:23:29 +0200

    added test util file

---
+++ __tests__/test-utils.ts
@@ -0,0 +1,40 @@
+import {
+  DEFAULT_REGEX,
+  CPP_REGEX,
+  CSHARP_REGEX
+} from '../src/index'
+
+let fs = require('fs')
+
+const cppPath = "./languages/cpp.cpp";
+const cpp = readFile(cppPath)
+
+const csharpPath = "./languages/csharp.cs";
+const csharp = readFile(csharpPath)
+console.log(cpp)
+
+const languages = new Map([
+  ["cpp", cpp],
+  ["csharp", csharp],
+]);
+
+const regexs = new Map([
+  ["cpp", CPP_REGEX],
+  ["csharp", CSHARP_REGEX],
+])
+
+export const testRegex = (name: string) => {
+  const languageExample = languages.get(name);
+  const regex = regexs.get(name);
+
+
+  return languageExample.match(regex)
+};
+
+function readFile(path) {
+  return fs.readFileSync(process.cwd() + "/__tests__/" + path).toString()
+}
+
+module.exports = {
+  testRegex
+};
