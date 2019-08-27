f158d776874cb7dfb4eb049264c987e30ea9325b
Ivan Verevkin <ivan.p.verevkin@gmail.com>
2019-08-27 16:24:17 +0200

    feat: added C# specs

---
+++ __tests__/csharp.ts
@@ -0,0 +1,17 @@
+import { testRegex } from "./test-utils";
+
+const languageName = 'csharp';
+
+describe("C#", () => {
+  it("returns `Hello World! program`", () => {
+    expect(testRegex(languageName)).toContain('/* Hello World! program */')
+  });
+
+  it("returns `// output`", () => {
+    expect(testRegex(languageName)).toContain(' // output')
+  });
+
+  it("returns `// commented line`", () => {
+    expect(testRegex(languageName)).toContain('\n// commented line')
+  });
+});
---
+++ __tests__/languages/csharp.cs
@@ -0,0 +1,12 @@
+/* Hello World! program */
+namespace HelloWorld
+{
+    class Hello {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!"); // output
+        }
+    }
+}
+
+// commented line
