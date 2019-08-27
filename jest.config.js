5f28d71018dec0ed6ddee0b603e5238ffe22286d
Ivan Verevkin <ivan.p.verevkin@gmail.com>
2019-08-27 16:20:17 +0200

    fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ["**/__tests__/**/*.test.ts"],
-  collectCoverageFrom: ["packages/**/src/**/*"],
+  collectCoverageFrom: ["src/index.ts"],
   globals: {
     'ts-jest': {
       tsConfig: './__tests__/tsconfig.test.json'
