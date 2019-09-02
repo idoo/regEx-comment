a6bb0b1965670e21aad2d34c9c1ee057c280a63a
Ivan Verevkin <ivan.p.verevkin@gmail.com>
2019-09-02 16:39:24 +0200

    chore: reordered structure

--- __tests__/test-utils.ts
+++ __tests__/test-utils.ts
@@ -17,51 +17,53 @@ import {
   TYPESCRIPT_REGEX
 } from "../lib/index";
 
+import { read } from "fs";
+
 let fs = require("fs");
 
-const cppPath = "languages/cpp.cpp";
+const cppPath = "./languages/cpp.cpp";
 const cpp = readFile(cppPath);
 
-const csharpPath = "languages/csharp.cs";
+const csharpPath = "./languages/csharp.cs";
 const csharp = readFile(csharpPath);
 
-const fortranPath = "languages/fortran.f";
+const fortranPath = "./languages/fortran.f";
 const fortran = readFile(fortranPath);
 
-const goPath = "languages/go.go";
+const goPath = "./languages/go.go";
 const go = readFile(goPath);
 
-const haskellPath = "languages/haskell.hs";
+const haskellPath = "./languages/haskell.hs";
 const haskell = readFile(haskellPath);
 
-const kotlinPath = "languages/kotlin.kt";
+const kotlinPath = "./languages/kotlin.kt";
 const kotlin = readFile(kotlinPath);
 
-const luaPath = "languages/lua.lua";
+const luaPath = "./languages/lua.lua";
 const lua = readFile(luaPath);
 
-const javaPath = "languages/java.java";
+const javaPath = "./languages/java.java";
 const java = readFile(javaPath);
 
-const javascriptPath = "languages/javascript.js";
+const javascriptPath = "./languages/javascript.js";
 const javascript = readFile(javascriptPath);
 
-const typescriptPath = "languages/typescript.ts";
+const typescriptPath = "./languages/typescript.ts";
 const typescript = readFile(typescriptPath);
 
-const perlPath = "languages/perl.pl";
+const perlPath = "./languages/perl.pl";
 const perl = readFile(perlPath);
 
-const phpPath = "languages/php.php";
+const phpPath = "./languages/php.php";
 const php = readFile(phpPath);
 
-const pythonPath = "languages/python.py";
+const pythonPath = "./languages/python.py";
 const python = readFile(pythonPath);
 
-const swiftPath = "languages/swift.swift";
+const swiftPath = "./languages/swift.swift";
 const swift = readFile(swiftPath);
 
-const rubyPath = "languages/ruby.rb";
+const rubyPath = "./languages/ruby.rb";
 const ruby = readFile(rubyPath);
 
 const languages = new Map([
